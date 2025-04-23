4dd8c5aa733ec66caad76f652aa2f7964abca3b7
wesleyhmartin <wesleyhmartin@googlemail.com>
2025-04-22 20:38:50 -0400

    bar

--- media/hotlinebin.php
+++ media/hotlinebin.php
@@ -1,12 +1,7 @@
 <?php
-header("Content-Type: application/xml");
-echo '<?xml version="1.0" encoding="UTF-8"?>';
+header("Content-Type: application/xml");  // Set content type to XML
+echo '<?xml version="1.0" encoding="UTF-8"?>';  // Add XML header
 ?>
 <Response>
     <Play>https://wesleymartin.net/media/hotline.mp3</Play>
-</Response>
-
-<?xml version="1.0" encoding="UTF-8"?>
-<Response>
-   <Play>https://wesleymartin.net/media/hotline.mp3</Play>
 </Response>
