28c7848d1f7441f7b37d03f2ebb96bb54a80f747
wesleyhmartin <wesleyhmartin@googlemail.com>
2025-04-22 21:39:54 -0400

    hotline

--- media/hotlinebin.php
+++ media/hotlinebin.php
@@ -1,10 +1,3 @@
-<?php
-// Ensure we send the right content type to Twilio
-header("Content-Type: application/xml");
-
-// Output the actual TwiML XML
-echo '<?xml version="1.0" encoding="UTF-8"?>';
-?>
 <Response>
-    <Play>https://wesleymartin.net/media/hotline.mp3</Play>
+<Play>https://wesleymartin.net/media/hotline.mp3</Play>
 </Response>
