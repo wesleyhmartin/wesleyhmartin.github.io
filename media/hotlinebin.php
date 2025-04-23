ded141857fce2dffc2fd007fc744c532910c2b40
wesleyhmartin <wesleyhmartin@googlemail.com>
2025-04-22 20:42:39 -0400

    Update hotlinebin.php

--- media/hotlinebin.php
+++ media/hotlinebin.php
@@ -1,6 +1,9 @@
 <?php
-header("Content-Type: application/xml");  // Set content type to XML
-echo '<?xml version="1.0" encoding="UTF-8"?>';  // Add XML header
+// Ensure we send the right content type to Twilio
+header("Content-Type: application/xml");
+
+// Output the actual TwiML XML
+echo '<?xml version="1.0" encoding="UTF-8"?>';
 ?>
 <Response>
     <Play>https://wesleymartin.net/media/hotline.mp3</Play>
